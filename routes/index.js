81a54831080fb8e510dfbc6065a59832a6fc58d3
Terrence Kuiper <github@terrencekuiper.com>
2020-01-28 16:23:45 -0800

    Add photo of me

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  res.render("index", { title: "Terrence Kuiper's Resume" });
+  res.render("index", { title: "Terrence Kuiper" });
 });
 
 module.exports = router;
