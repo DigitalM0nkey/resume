5dfd276ebb391e1a5b5de0c0802b3f00e5bc94d1
Terrence Kuiper <github@terrencekuiper.com>
2020-01-30 13:25:15 -0800

    Add rotating background to profile pic

--- public/javascripts/dayNight.js
+++ public/javascripts/dayNight.js
@@ -8,9 +8,8 @@ const dayMode = () => {
     "border-style: inset; color: #424242";
   document.getElementById("nightMode").style =
     "border-style: outset; color: #2196f3";
-  document.getElementById("switch").style =
-    "background-color: #424242; border-color: #616161";
   $(".dayNight").css("color", "#b2e8ff");
+  $("#profile").css("border-color", "#f29b2f");
 };
 
 const nightMode = () => {
@@ -22,7 +21,6 @@ const nightMode = () => {
   document.getElementById("dayMode").style = "border-style: outset";
   document.getElementById("nightMode").style =
     "border-style: inset; color:#424242";
-  document.getElementById("switch").style =
-    "background-color: #424242; border-color: #616161";
   $(".dayNight").css("color", "gainsboro");
+  $("#profile").css("border-color", "#582ffc");
 };
