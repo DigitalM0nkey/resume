b90a757af23468baab2918f4db9ec13df253bf92
Terrence Kuiper <github@terrencekuiper.com>
2020-01-30 11:50:09 -0800

    Update text to be more ledgable in day/night mode

--- public/javascripts/dayNight.js
+++ public/javascripts/dayNight.js
@@ -1,7 +1,7 @@
 const dayMode = () => {
   console.log("IMAGE", document.body.style.backgroundImage);
   document.getElementById("body").style.backgroundImage =
-    "url(/images/dayMode.jpg)";
+    "url(https://farm3.staticflickr.com/2841/33833704511_94d448186e_k.jpg)";
   document.getElementById("profileBackgroundDay").style = "display: flex";
   document.getElementById("profileBackgroundNight").style = "display: none";
   document.getElementById("dayMode").style =
@@ -10,13 +10,13 @@ const dayMode = () => {
     "border-style: outset; color: #2196f3";
   document.getElementById("switch").style =
     "background-color: #424242; border-color: #616161";
-  $(".dayNight").css("color", "#673ab7");
+  $(".dayNight").css("color", "#b2e8ff");
 };
 
 const nightMode = () => {
   console.log("IMAGE", document.body.style.backgroundImage);
   document.getElementById("body").style.backgroundImage =
-    "url(/images/nightMode.jpg)";
+    "url(/images/night.jpg)";
   document.getElementById("profileBackgroundDay").style = "display: none";
   document.getElementById("profileBackgroundNight").style = "display: flex";
   document.getElementById("dayMode").style = "border-style: outset";
