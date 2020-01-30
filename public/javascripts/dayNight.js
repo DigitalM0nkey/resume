ab9a8f5c3e0dca57bff0a82ff07694bafef74a5c
Terrence Kuiper <github@terrencekuiper.com>
2020-01-30 15:11:04 -0800

    Refactor dayNight in JQuery

--- public/javascripts/dayNight.js
+++ public/javascripts/dayNight.js
@@ -1,26 +1,25 @@
 const dayMode = () => {
-  console.log("IMAGE", document.body.style.backgroundImage);
-  document.getElementById("body").style.backgroundImage =
-    "url(https://farm3.staticflickr.com/2841/33833704511_94d448186e_k.jpg)";
-  document.getElementById("profileBackgroundDay").style = "display: flex";
-  document.getElementById("profileBackgroundNight").style = "display: none";
-  document.getElementById("dayMode").style =
-    "border-style: inset; color: #424242";
-  document.getElementById("nightMode").style =
-    "border-style: outset; color: #2196f3";
+  $("body").css(
+    "background-image",
+    "url(https://farm3.staticflickr.com/2841/33833704511_94d448186e_k.jpg)"
+  );
+  $("#profileBackgroundDay").css("display", "flex");
+  $("#profileBackgroundNight").css("display", "none");
+  $("#dayMode").css({ borderStyle: "inset", color: "#424242" });
+  $("#nightMode").css({ borderStyle: "outset", color: "#2196f3" });
   $(".dayNight").css("color", "#b2e8ff");
   $("#profile").css("border-color", "#f29b2f");
 };
 
 const nightMode = () => {
-  console.log("IMAGE", document.body.style.backgroundImage);
-  document.getElementById("body").style.backgroundImage =
-    "url(/images/night.jpg)";
-  document.getElementById("profileBackgroundDay").style = "display: none";
-  document.getElementById("profileBackgroundNight").style = "display: flex";
-  document.getElementById("dayMode").style = "border-style: outset";
-  document.getElementById("nightMode").style =
-    "border-style: inset; color:#424242";
+  $("body").css(
+    "background-image",
+    "url(https://images.pexels.com/photos/713664/pexels-photo-713664.jpeg)"
+  );
+  $("#profileBackgroundDay").css("display", "none");
+  $("#profileBackgroundNight").css("display", "flex");
+  $("#dayMode").css({ borderStyle: "outset", color: "yellow" });
+  $("#nightMode").css({ borderStyle: "inset", color: "#424242" });
   $(".dayNight").css("color", "gainsboro");
   $("#profile").css("border-color", "#582ffc");
 };
